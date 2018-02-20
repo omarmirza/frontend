4be0ac9251cf1d529957330b709b700ca8c71ae9
omarmirza <omar.mirza@ubizie.com>
2018-02-19 22:10:28 -0600

    Added Google Sign In Button

--- src/components/forms/UCreateAcc.js
+++ src/components/forms/UCreateAcc.js
@@ -7,6 +7,7 @@ import { Section } from '../common';
 import axios from 'axios';
 
 class UCreateAcc extends Component {
+  state = { user: '' }
   componentDidMount() {
     GoogleSignin.hasPlayServices({ autoResolve: true }).then(() => {
     // play services are available. can now configure library
@@ -18,19 +19,36 @@ class UCreateAcc extends Component {
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
       //iosClientId: <FROM DEVELOPER CONSOLE>, // only for iOS
       webClientId: "406581460719-uaqdo755j1mfitihjrrbn7ila1r8hdha.apps.googleusercontent.com", // client ID of type WEB for your server (needed to verify user ID and offline access)
-      offlineAccess: true // if you want to access Google API on behalf of the user FROM YOUR SERVER
-      //hostedDomain: '' // specifies a hosted domain restriction
-      //forceConsentPrompt: true // [Android] if you want to show the authorization prompt at each login
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      hostedDomain: '', // specifies a hosted domain restriction
+      forceConsentPrompt: true,  // [Android] if you want to show the authorization prompt at each login
       //accountName: '' // [Android] specifies an account name on the device that should be used
     })
     .then(() => {
   // you can now call currentUserAsync()
-  });
-
   GoogleSignin.currentUserAsync().then((user) => {
       console.log('USER', user);
       this.setState({ user: user });
     }).done();
+  });
+
+  //GoogleSignin.currentUserAsync().then((user) => {
+  //    console.log('USER', user);
+  //    this.setState({ user: user });
+  //  }).done();
+
+  }
+
+  handle() {
+    GoogleSignin.signIn()
+      .then((user) => {
+        console.log(user);
+        this.setState({ user: user });
+      })
+      .catch((err) => {
+        console.log('WRONG SIGNIN', err);
+      })
+      .done();
   }
 
 
@@ -54,7 +72,7 @@ class UCreateAcc extends Component {
               style={{ width: 300, height: 48 }}
               size={GoogleSigninButton.Size.Wide}
               color={GoogleSigninButton.Color.Light}
-              onPress={this.componentDidMount.bind(this)}
+              onPress={this.handle.bind(this)}
               />
           </View>
         </Section>
